718d9215655542bdf9380f46fd413e4740731366
stefian <v_ianta@yahoo.com>
2019-06-16 16:49:59 -0400

    added async for callback task functions in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+gulp.task('default', async function() {
     console.log('Hooray - you created a gulp task.');
 });
+
+gulp.task('html', async () => {
+    console.log('Imagine something useful being done to your HTML here.');
+})
